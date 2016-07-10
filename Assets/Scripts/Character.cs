5cf5337169f3e0eef4f1637e8b47713dcf69326f
Nejc Anclin <nejc.anclin@gmail.com>
2016-07-10 21:44:06 +0200

    character accelerate

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class Character : MonoBehaviour {
 	
-	[SerializeField] private float Speed = 20;
+	[SerializeField] private float Speed;
+	[SerializeField] private float TopSpeed;
 
 	public Vector2 Pos{ get; private set; }
 	public Vector2 TargetPos;
@@ -13,9 +14,10 @@ public class Character : MonoBehaviour {
 	}
 
 	void FixedUpdate(){
-		
-		Vector2 dir = (TargetPos - Pos).normalized;
-		Pos += dir * Time.fixedDeltaTime * Speed;
+		Vector2 moveVector = (TargetPos - Pos) * Time.fixedDeltaTime * Speed;
+		moveVector = Vector2.ClampMagnitude( moveVector, TopSpeed );
+		Pos += moveVector;
+			
 //		Pos = ClampPosToMapSpace( BrushMap, Pos );
 
 		transform.position = Pos;
